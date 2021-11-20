dedeb65ffbf5945275dad8e9250e43fbb211ea96
YounSangWoo <tkddn0811@gmail.com>
2021-11-20 15:25:50 +0900

    DFS이분그래프

--- 백준/jungle_week03/7_트리의 부모 찾기.py
+++ 백준/jungle_week03/7_트리의 부모 찾기.py
@@ -1,10 +1,13 @@
-# https: // www.acmicpc.net/problem/11725
+# https://www.acmicpc.net/problem/11725
 """ 
 문제
-루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
+루트 없는 트리가 주어진다.
+ 이때, 트리의 루트를 1이라고 정했을 때,
+  각 노드의 부모를 구하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
+첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 
+둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
 7
 1 6
 6 3
@@ -25,38 +28,28 @@
  """
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+edges = []
+N = int(input())
+graph = [[] for _ in range(N + 1)]
+parents = [0 for _ in range(N+1)]
 
-def find(parent, x):
-    if parent[x] == x:
-        return x
-    parent[x] = find(parent, parent[x])
-    return parent[x]
-
-
-def union(parent, a, b):
-    rootA = find(parent, a)
-    rootB = find(parent, b)
-
-    if rootA < rootB:
-        parent[rootB] = rootA
-    else:
-        parent[rootA] = rootB
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    edges.append((a, b))
+    # graph[a][b] = graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
 
-N= int(input())
-edges =[]
-parent = [0]*N+1
+def dfs(start, graph, parents):
+    for i in graph[start]:  # start = 부모
+        if parents[i] == 0:
+            parents[i] = start
+            dfs(i, graph, parents)
 
 
+dfs(1, graph, parents)
 
-#간선정보입력
-for i in range(N-1):
-    a, b = map(int,input().split)
-    edges.append((a,b))
-
-edges.sort(key = lambda x: -x[0])
-
-for edge in edges:
-    a, b = edge
-    # 사이클이 발생하지 않는 경우에만 집합에 포함(=연결한다.)
-    if find(parent, a) != find(parent, b)
+for i in range(2, len(parents)):
+    print(parents[i])
