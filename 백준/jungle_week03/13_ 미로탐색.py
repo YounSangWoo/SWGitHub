28552e1fe9e161d2d580ab492be306c20660ed7e
YounSangWoo <tkddn0811@gmail.com>
2021-11-22 16:49:37 +0900

    미로탐색

--- 백준/jungle_week03/13_ 미로탐색.py
+++ 백준/jungle_week03/13_ 미로탐색.py
@@ -1,5 +1,11 @@
 # https://www.acmicpc.net/problem/2178
 """ 
+미로에서 1은 이동할 수 있는 칸을 나타내고,
+0은 이동할 수 없는 칸을 나타낸다. 
+이러한 미로가 주어졌을 때, (1, 1)에서 출발하여
+(N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 
+한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+
 입력
 첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다.
 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
@@ -17,6 +23,9 @@
 15
 
 """
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 from collections import deque
 
 N, M = map(int, input().split())
@@ -24,12 +33,14 @@ N, M = map(int, input().split())
 graph = []
 
 for _ in range(N):
-  graph.append(list(map(int, input())))
+  graph.append(list(map(int, input().strip())))
 
 # 너비 우선 탐색
+
+
 def bfs(x, y):
   # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
-  dx = [-1, 1, 0, 0] 
+  dx = [-1, 1, 0, 0]
   dy = [0, 0, -1, 1]
 
   # deque 생성
@@ -38,7 +49,7 @@ def bfs(x, y):
 
   while queue:
     x, y = queue.popleft()
-    
+
     # 현재 위치에서 4가지 방향으로 위치 확인
     for i in range(4):
       nx = x + dx[i]
@@ -47,17 +58,18 @@ def bfs(x, y):
       # 위치가 벗어나면 안되기 때문에 조건 추가
       if nx < 0 or nx >= N or ny < 0 or ny >= M:
         continue
-      
+
       # 벽이므로 진행 불가
       if graph[nx][ny] == 0:
         continue
-      
+
       # 벽이 아니므로 이동
       if graph[nx][ny] == 1:
         graph[nx][ny] = graph[x][y] + 1
         queue.append((nx, ny))
-  
+
   # 마지막 값에서 카운트 값을 뽑는다.
   return graph[N-1][M-1]
+
 
 print(bfs(0, 0))
--- 백준/jungle_week03/trest.py
+++ 백준/jungle_week03/trest.py
@@ -1,55 +1,6 @@
 import sys
-sys.setrecursionlimit(100000)
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
-
-def dfs(r, c, cnt):
-    for i in range(4):
-        nr = r+dr[i]
-        nc = c+dc[i]
-        if nr >= N or nr < 0 or nc >= M or nc < 0:
-            continue
-        if arr[nr][nc] == 0 and visited[nr][nc] == 0:
-            cnt += 1
-        if arr[nr][nc] and not visited[nr][nc]:
-            visited[nr][nc] = 1
-            dfs(nr, nc, 0)
-
-    if cnt > 0:
-        arr[r][c] -= cnt
-        if arr[r][c] < 0:
-            arr[r][c] = 0
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+from collections import deque
 
 
-def check():
-    global ccnt, flag
-    for i in range(1, N-1):
-        for j in range(1, M-1):
-            if arr[i][j] and not visited[i][j]:
-                flag = 0
-                ccnt += 1
-                if ccnt > 1:
-                    return
-                visited[i][j] = 1
-                dfs(i, j, 0)
-
-
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-year = 0
-while True:
-    flag = 1
-    ccnt = 0
-    visited = [[0] * M for _ in range(N)]
-    check()
-    if ccnt > 1:
-        break
-    if flag:
-        year = 0
-        break
-    year += 1
-
-print(year)
