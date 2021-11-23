726256901f2eefc9cb6b3bc0b40d14c9554676e7
YounSangWoo <tkddn0811@gmail.com>
2021-11-23 19:48:59 +0900

    줄세우기 _위상정렬

--- 백준/jungle_week03/trest.py
+++ 백준/jungle_week03/trest.py
@@ -1,46 +1,34 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
-dx = [1, -1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
-def bfs():
-    while q:
-        a, b, c = q.popleft()
-        visit[c][a][b] = 1
-        for i in range(6):
-            x = a + dx[i]
-            y = b + dy[i]
-            z = c + dz[i]
-            if 0 <= x < n and 0 <= y < m and 0 <= z < h and s[z][x][y] == 0 and visit[z][x][y] == 0:
-                q.append([x, y, z])
-                s[z][x][y] = s[c][a][b] + 1
-                visit[z][x][y] = 1
+from collections import deque
 
-m, n, h = map(int, input().split())
-s = [[] for i in range(h)]
-visit = [[[0 for i in range(m)] for i in range(n)] for i in range(h)]
-q = deque()
-isTrue = False
-st = False
-for i in range(h):
-    for j in range(n):
-        s[i].append(list(map(int, input().split())))
-for z in range(h):
-    for x in range(n):
-        for y in range(m):
-            if s[z][x][y] == 1:
-                q.append([x, y, z])
-                
-bfs()
-max_num = 0
-for z in range(h):
-    for x in range(n):
-        for y in range(m):
-            if s[z][x][y] == 0:
-                isTrue = True
-            max_num = max(max_num, s[z][x][y])
-if isTrue == True:
-    print(-1)
-else:
-    print(max_num - 1)
+
+n, m = map(int, sys.stdin.readline().split())
+h_graph = [[] for _ in range (n+1)]
+l_graph = [[] for _ in range (n+1)]
+for _ in range(m):
+    h, l = map(int, sys.stdin.readline().split())
+    h_graph[l].append(h)
+    l_graph[h].append(l)
+
+def dfs(num, g):
+    stack = deque()
+    stack.append(num)
+    visitied = [False for _ in range(n+1)]
+    cnt = 0
+    while stack:
+        cur = stack.pop()
+        visitied[cur] = True
+        for item in g[cur]:
+            if visitied[item] == False:
+                stack.append(item)
+                cnt += 1
+    return cnt
+limit = n//2#n은 홀수  2
+limit_over=0
+for i in range(1, n+1):
+    if dfs(i, h_graph) > limit: # 절반 + 1 보다 클경우
+        limit_over += 1
+    if dfs(i, l_graph) > limit:
+        limit_over += 1
+
+print(limit_over)
