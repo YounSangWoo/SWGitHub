5505fb61d7d497392d21a4d60010b75348547e52
YounSangWoo <tkddn0811@gmail.com>
2021-11-27 13:09:40 +0900

    가장 긴 증가하는 부분수열

---
+++ 백준/jungle_week04/7번 _가장 긴 증가하는 부분수열.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/11053
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, (input().split()[:N])))
+
+dp = [0]*N
+for i in range(N):
+    for j in range(i):
+        if nums[i] > nums[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
